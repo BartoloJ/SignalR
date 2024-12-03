1b82a978b6411cef4ff8393fcc77a33fe1f8de8d
Jonathan Bartolo <jonathan.e.bartolo@gmail.com>
2024-12-02 22:40:25 -0800

    Initial Commit

---
+++ Homework/Program.cs
@@ -0,0 +1,10 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<ChatHub>("/chathub");
+app.UseStaticFiles();
+
+app.Run();
